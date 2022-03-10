7682bc1fd6e8f476397c5e4530d252cd6c32b1f8
Yiyi Tao <yiyi@yiyis-mbp.acasmart.jh.edu>
2022-03-10 18:06:44 -0500

    download_img

--- code/ai-compute-sever/util.py
+++ code/ai-compute-sever/util.py
@@ -14,6 +14,12 @@ def generate_random_name(len):
     res = res + '.jpg'
     return res
 
+def url_to_image(url, readFlag=cv2.IMREAD_COLOR):
+    resp = urlopen(url)
+    image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    image = cv2.imdecode(image, readFlag)
+    return image
+
 def upload_image(img_dir):
     s3 = boto3.client('s3')
     name = generate_random_name(16)
@@ -26,11 +32,18 @@ def upload_image(img_dir):
                                 ExpiresIn=7200)
     return name, url
 
-def url_to_image(url, readFlag=cv2.IMREAD_COLOR):
-    resp = urlopen(url)
-    image = np.asarray(bytearray(resp.read()), dtype="uint8")
-    image = cv2.imdecode(image, readFlag)
-    return image
+def download_image(img_name):
+    s3 = boto3.client('s3')
+    url = s3.generate_presigned_url('get_object',
+                                Params={
+                                    'Bucket': 'aifun',
+                                    'Key': img_name,
+                                },                                  
+                                ExpiresIn=7200)
+    img = url_to_image(url)
+    cv2.imwrite(img_name, img)
+
+
 
 
 
