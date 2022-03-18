e163918a1879ac4d469e5d737a6bd562f75609f1
Gnosil <90169347+Gnosil@users.noreply.github.com>
2022-03-18 16:51:13 -0400

    Upload model Package

---
+++ code/backend/TTS_model_package.py
@@ -0,0 +1,30 @@
+import os
+import torch
+import time
+
+
+from TTS.tts.utils.generic_utils import setup_model
+from TTS.utils.io import load_config
+from TTS.tts.utils.text.symbols import symbols, phonemes
+from TTS.utils.audio import AudioProcessor
+from TTS.tts.utils.synthesis import synthesis
+
+def tts(model, text, CONFIG, use_cuda, ap, use_gl, figures=True):
+    t_1 = time.time()
+    waveform, alignment, mel_spec, mel_postnet_spec, stop_tokens, inputs = synthesis(model, text, CONFIG, use_cuda, ap, speaker_id, style_wav=None,
+                                                                             truncated=False, enable_eos_bos_chars=CONFIG.enable_eos_bos_chars)
+    # mel_postnet_spec = ap.denormalize(mel_postnet_spec.T)
+    if not use_gl:
+        waveform = vocoder_model.inference(torch.FloatTensor(mel_postnet_spec.T).unsqueeze(0))
+        waveform = waveform.flatten()
+    if use_cuda:
+        waveform = waveform.cpu()
+    waveform = waveform.numpy()
+    rtf = (time.time() - t_1) / (len(waveform) / ap.sample_rate)
+    tps = (time.time() - t_1) / len(waveform)
+    print(waveform.shape)
+    print(" > Run-time: {}".format(time.time() - t_1))
+    print(" > Real-time factor: {}".format(rtf))
+    print(" > Time per step: {}".format(tps))
+     
+    return alignment, mel_postnet_spec, stop_tokens, waveform
