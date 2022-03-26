ef444e8c104e213eddbaa1b268a1f764131f3a5c
SherryLee1998 <li.jingyan@northeastern.edu>
2022-03-26 00:29:30 -0500

    post api, but backend block request

--- code/frontend/front/src/components/Profile.js
+++ code/frontend/front/src/components/Profile.js
@@ -1,37 +1,41 @@
 
-import React,{useState,useContext} from 'react';
-import {message, Input,Form} from 'antd';
-import {CheckCircleOutlined,CloseCircleOutlined} from '@ant-design/icons';
+import React, { useState, useContext } from 'react';
+import { message, Input, Form } from 'antd';
+import { CheckCircleOutlined, CloseCircleOutlined } from '@ant-design/icons';
 import UploadPic from './UploadPic';
 import '../style/Profile.css';
-import {Row, Col, Button} from 'react-bootstrap';
+import { Row, Col, Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import "../bootstrap-4.3.1-dist/css/bootstrap.min.css";
-import {LoginContext} from '../context/AuthProvider';
+import { LoginContext } from '../context/AuthProvider';
 import { useCookies } from 'react-cookie';
 
-const compareToFirstPassword = ({getFieldValue}) => ({
-    validator(rule, value){
-      if (getFieldValue('password')=== value) return Promise.resolve();
-      return Promise.reject("Two passwords that you enter is inconsistent!")
+const compareToFirstPassword = ({ getFieldValue }) => ({
+    validator(rule, value) {
+        if (getFieldValue('password') === value) return Promise.resolve();
+        return Promise.reject("Two passwords that you enter is inconsistent!")
     }
-  });
+});
 
 function Profile(props) {
     props = props.props
     // const {user,setUser,email,setEmail} = useContext(LoginContext);
-    const [cookie, setCookie] = useCookies(['access_token', 'refresh_token','name','email'])
+    const [cookie, setCookie] = useCookies(['token', 'refresh_token', 'name', 'email', 'user_id'])
     const [name, setName] = useState(cookie.name);
     const [email, setEmail] = useState(cookie.email);
     const [password, setPassword] = useState('');
     const [oriPsw, setOriPsw] = useState('');
     const [showInputEmail, setshowInputEmail] = useState(false);
     const { avatarimg } = useContext(LoginContext);
-    const [showEditPsw,setShowEditPsw] = useState(false)
+    const [showEditPsw, setShowEditPsw] = useState(false)
     const [showInputName, setshowInputName] = useState(false);
     const [pic, setPic] = useState(props.pic);
- 
+    var user_id = localStorage.getItem('global_userID');
+    var globla_token = localStorage.getItem('global_token');
+    console.log(user_id);
+    console.log(globla_token);
+
     // Edit Name
     const handleEditName = () => {
         console.log(1);
@@ -40,40 +44,42 @@ function Profile(props) {
     };
     // TODO: Interaction with backend
     const handleAffirmName = async () => {
-        if (name != cookie.name){
-            try{
-                let url = 'https://server-demo.ai-for-fun-backend.com/changeinfo/:'+cookie.access_token
-                
-                const response = await fetch(url,{
+        if (name != cookie.name) {
+            try {
+                let url = 'https://server-demo.ai-for-fun-backend.com/changeinfo/' + user_id
+
+                const response = await fetch(url, {
                     method: 'POST',
-                    headers: {'Content-Type':'application/json'},
-                    body: JSON.stringify({
-                        "new_name":name
-                    })
+                    headers: {
+                        //'content-Type': 'application/json',
+                        'token': globla_token
+                    },
+                    body: {
+                        "new_name": name.toString()
+                    }
                 });
-                console.log(response.status)
-    
-              if (response.status == 200){
-                  const content = await response.json();
-                  let expires = new Date();
-                  expires.setTime(expires.getTime() + (30*60*1000));
-                  setCookie('name', content.name, { path: '/',  expires});
-              }
-              else {
-                  console.log('request failed', response);
-                  message.error('request failed')
-              } 
-              }catch(err){
-                  if (err.response?.status ===500){
-                      message.error('Missing Email or Password');
-                  }else{
-                      message.error('Login Failed!');
-                  }
-              }
-        }else{
+
+                if (response.status == 200) {
+                    const content = await response.json();
+                    let expires = new Date();
+                    expires.setTime(expires.getTime() + (30 * 60 * 1000));
+                    setCookie('name', content.name, { path: '/', expires });
+                }
+                else {
+                    console.log('request failed', response);
+                    message.error('request failed')
+                }
+            } catch (err) {
+                if (err.response?.status === 500) {
+                    message.error('Missing Email or Password');
+                } else {
+                    message.error('Login Failed!');
+                }
+            }
+        } else {
             message.error("Please input a new name")
         }
-        
+
 
     };
     const handleCloseName = () => {
@@ -96,161 +102,185 @@ function Profile(props) {
     const handleClosePsw = () => {
         setShowEditPsw(false)
     }
-    const handleAffirmPsw = async() => {
-        if (password && oriPsw){
-            try{
-                let url = 'https://server-demo.ai-for-fun-backend.com/changeinfo/:'+cookie.access_token
-                const response = await fetch(url,{
+    const handleAffirmPsw = async () => {
+        if (password && oriPsw) {
+            try {
+                let url = 'https://server-demo.ai-for-fun-backend.com/changeinfo/' + user_id
+                console.log(url);
+                const response = await fetch(url, {
                     method: 'POST',
-                    headers: {'Content-Type':'application/json'},
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'token': globla_token
+                    },
                     body: JSON.stringify({
-                        "old_password":oriPsw,
-                        "new_password":password
+                        "old_password": oriPsw,
+                        "new_password": password
                     })
                 });
                 console.log(response.status)
-    
-              if (response.status == 200){
-                  const content = await response.json();
-                  let expires = new Date();
-                  expires.setTime(expires.getTime() + (30*60*1000));
-                  setCookie('name', content.name, { path: '/',  expires});
-              }
-              else {
-                  console.log('request failed', response);
-                  setName(cookie.name)
-                  message.error('request failed')
-              } 
-              }catch(err){
-                  if (err.response?.status ===500){
-                      message.error('Missing Email or Password');
-                  }else{
-                      message.error('Login Failed!');
-                  }
-              }
-        }else{
+
+                if (response.status == 200) {
+                    const content = await response.json();
+                    let expires = new Date();
+                    expires.setTime(expires.getTime() + (30 * 60 * 1000));
+                    setCookie('name', content.name, { path: '/', expires });
+                }
+                else {
+                    console.log('request failed', response);
+                    setName(cookie.name)
+                    message.error('request failed')
+                }
+            } catch (err) {
+                if (err.response?.status === 500) {
+                    message.error('Missing Email or Password');
+                } else {
+                    message.error('Login Failed!');
+                }
+            }
+        } else {
             message.error("Please input password")
         }
-        
+        // fetch('https://server-demo.ai-for-fun-backend.com/changeinfo/' + user_id, {
+        //     method: 'POST',
+        //     headers: {
+        //         'content-type': 'application/json',
+        //         'token': globla_token
+        //     },
+        //     body: {
+        //         "old_password": oriPsw,
+        //         "new_password": password,
+        //     }
+        // })
+        //     .then(response => {
+        //         console.log(response)
+        //     })
+        //     .catch(err => {
+        //         console.log(err)
+        //     })
+
     }
-    
+
     return (
-        <Container style={{minHeight: '100vh'}}>
+        <Container style={{ minHeight: '100vh' }}>
             <Row className='pt-3'>
-            <Col md={4} style={{backgroundColor: 'whitesmoke'}} className="mr-1">
-                <Row className='mt-4'>
-                    <div style={{width: 180}} className="mx-auto">
-                    <Image roundedCircle src={pic} fluid />
-                    </div>
-                </Row>
-                <br/>
-                <Row>
-                    <p className="font-weight-bold text-center">{cookie.name}</p>
-                    <p className="text-center">{cookie.email}</p>
-                    
-                </Row>
-                {showEditPsw?
-                <div>
-                <center>
-                <Button variant="outline-dark" size="sm" onClick={handleClosePsw} >Cancle</Button>
-                <hr/>
-                </center>
-                <Form>
-                <Form.Item
-                    label="Original"
-                    name="original"
-                    rules={[
-                    {required: true,
-                    message: 'Please input your original password!',
-                    },
-                ]}
-                >
-                    <Input.Password  onChange={e=> setOriPsw(e.target.value)} />
-                </Form.Item>
-                <Form.Item
-                    label="Password"
-                    name="password"
-                    rules={[
-                    {required: true,
-                    message: 'Please input your password!',
-                    },
-                ]}
-                >
-                <Input.Password  onChange={e=> setPassword(e.target.value)} />
-                </Form.Item>
-                <Form.Item
-                    label="Confirm "
-                    name="confirm"
-                    rules={[
-                    {required: true,
-                    message: 'Please confirm your password!',
-                    },
-                    compareToFirstPassword,
-                ]}
-                >
-                <Input.Password  onChange={e=> setPassword(e.target.value)} />
-                </Form.Item>
-                <Form.Item>
-                <center>
-                <Button variant="outline-dark" size="sm" onClick={handleAffirmPsw} >Confirm</Button>
-                </center>
-                </Form.Item>
-                </Form>
-                </div>
-               
-            :
+                <Col md={4} style={{ backgroundColor: 'whitesmoke' }} className="mr-1">
+                    <Row className='mt-4'>
+                        <div style={{ width: 180 }} className="mx-auto">
+                            <Image roundedCircle src={pic} fluid />
+                        </div>
+                    </Row>
+                    <br />
+                    <Row>
+                        <p className="font-weight-bold text-center">{cookie.name}</p>
+                        <p className="text-center">{cookie.email}</p>
 
-            <center>
-                <Button variant="outline-dark" size="sm" onClick={handleEditPsw} >Edit Password</Button>
-                <hr/>
-            </center>
+                    </Row>
+                    {showEditPsw ?
+                        <div>
+                            <center>
+                                <Button variant="outline-dark" size="sm" onClick={handleClosePsw} >Cancle</Button>
+                                <hr />
+                            </center>
+                            <Form>
+                                <Form.Item
+                                    label="Original"
+                                    name="original"
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message: 'Please input your original password!',
+                                        },
+                                    ]}
+                                >
+                                    <Input.Password onChange={e => setOriPsw(e.target.value)} />
+                                </Form.Item>
+                                <Form.Item
+                                    label="Password"
+                                    name="password"
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message: 'Please input your password!',
+                                        },
+                                    ]}
+                                >
+                                    <Input.Password onChange={e => setPassword(e.target.value)} />
+                                </Form.Item>
+                                <Form.Item
+                                    label="Confirm "
+                                    name="confirm"
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message: 'Please confirm your password!',
+                                        },
+                                        compareToFirstPassword,
+                                    ]}
+                                >
+                                    <Input.Password onChange={e => setPassword(e.target.value)} />
+                                </Form.Item>
+                                <Form.Item>
+                                    <center>
+                                        <Button variant="outline-dark" size="sm" onClick={handleAffirmPsw} >Confirm</Button>
+                                    </center>
+                                </Form.Item>
+                            </Form>
+                        </div>
 
-                
-            }
-                 
-            </Col>
-            <Col md={7}>
-            <Row style={{backgroundColor: 'whitesmoke'}}>
-                <Row className='mt-4' >
-                    <Col md={9}>
-                       <UploadPic />
-                    </Col>
-                    <Col md={3} className='pt-4'>
-                        <Button variant="outline-dark">Submit</Button>
-                    </Col>
-                </Row>
-                <br />
-            {!showInputName?
-                <Row className='mt-4 md-4'>
-                    <Col md={9}>
-                        <p>{name}</p>
-                    </Col>
-                    <Col md={3}>
-                        <Button variant="outline-dark" onClick={handleEditName}>Edit</Button>
-                    </Col>
-                </Row>
-                :
-                <Row className='mt-4 md-4'>
-                    <Col md = {9} sm = {5}>
-                        <Form.Item
-                        name="editName"
-                        >
-                        <Input  onChange={e=> setName(e.target.value)} placeholder="Enter Name"/>
-                        </Form.Item>
-                    </Col>
-                    <Col md = {3} sm = {7}>
-                        <CheckCircleOutlined onClick={handleAffirmName}/>
-                        <CloseCircleOutlined onClick={handleCloseName} />
-                    </Col>
-                </Row>
-            }
-            </Row>
-            <Row className="mt-1" style={{backgroundColor: 'whitesmoke',minHeight: '80vh'}}>
-                <h2>My work</h2>
-            </Row>
-            <div>
+                        :
 
-            {/* {!showInputEmail?
+                        <center>
+                            <Button variant="outline-dark" size="sm" onClick={handleEditPsw} >Edit Password</Button>
+                            <hr />
+                        </center>
+
+
+                    }
+
+                </Col>
+                <Col md={7}>
+                    <Row style={{ backgroundColor: 'whitesmoke' }}>
+                        <Row className='mt-4' >
+                            <Col md={9}>
+                                <UploadPic />
+                            </Col>
+                            <Col md={3} className='pt-4'>
+                                <Button variant="outline-dark">Submit</Button>
+                            </Col>
+                        </Row>
+                        <br />
+                        {!showInputName ?
+                            <Row className='mt-4 md-4'>
+                                <Col md={9}>
+                                    <p>{name}</p>
+                                </Col>
+                                <Col md={3}>
+                                    <Button variant="outline-dark" onClick={handleEditName}>Edit</Button>
+                                </Col>
+                            </Row>
+                            :
+                            <Row className='mt-4 md-4'>
+                                <Col md={9} sm={5}>
+                                    <Form.Item
+                                        name="editName"
+                                    >
+                                        <Input onChange={e => setName(e.target.value)} placeholder="Enter Name" />
+                                    </Form.Item>
+                                </Col>
+                                <Col md={3} sm={7}>
+                                    <CheckCircleOutlined onClick={handleAffirmName} />
+                                    <CloseCircleOutlined onClick={handleCloseName} />
+                                </Col>
+                            </Row>
+                        }
+                    </Row>
+                    <Row className="mt-1" style={{ backgroundColor: 'whitesmoke', minHeight: '80vh' }}>
+                        <h2>My work</h2>
+                    </Row>
+                    <div>
+
+                        {/* {!showInputEmail?
                 <Row>
                     <Col md={9}>
                         <p>{email}</p>
@@ -273,10 +303,10 @@ function Profile(props) {
                     </Col>
                 </Row>
             } */}
-            </div>
-            </Col>
+                    </div>
+                </Col>
             </Row>
-      </Container>
+        </Container>
     )
 }
 export default Profile;
