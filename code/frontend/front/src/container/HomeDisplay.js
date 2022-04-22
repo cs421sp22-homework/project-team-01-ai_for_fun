37f32e42fe79643e446313bcfbd4f489a73d198f
SherryLee1998 <li.jingyan@northeastern.edu>
2022-04-22 16:38:09 -0400

    modify home page

--- code/frontend/front/src/container/HomeDisplay.js
+++ code/frontend/front/src/container/HomeDisplay.js
@@ -8,19 +8,20 @@ function HomeDisplay() {
       className='p-5 bg-image'
       style={{ backgroundImage: "url('/images/header.jpg')", height: 760 }}
     >
-
-      <div style={{ marginTop: '10%', marginLeft: '7%' }}>
-        <div>
-          <font face="Arial" size="+4" color="white"> <strong>AI FOR FUN </strong></font>
-          <br></br>
-          <font face="Verdana" size="+2" color="white"> Our application aims to provide the best AI experience. </font>
-          <br></br>
-          <font face="Verdana" size="+2" color="white"> Enjoy the fun brought by AI technology via website</font>
-        </div>
-        <div>
-          <Button size="lg" href="/AI_face" style={{ marginTop: 20, marginRight: 10, backgroundColor: "rgb(69,113,182)", borderColor: 'rgba(0,0,0,0)' }}>AI Face</Button>{' '}
-          <Button size="lg" href="/AI_text" style={{ marginTop: 20, marginRight: 10, backgroundColor: "rgb(69,113,182)", borderColor: 'rgba(0,0,0,0)' }}>AI Voice</Button>{' '}
-          <Button size="lg" href="/AI_style" style={{ marginTop: 20, marginRight: 10, backgroundColor: "rgb(69,113,182)", borderColor: 'rgba(0,0,0,0)' }}>AI Style</Button>{' '}
+      <div style={{ width: '50%', marginTop: '10%' }}>
+        <div style={{ marginTop: '10%', marginLeft: '5%' }}>
+          <div>
+            <font face="Arial" size="+4" color="white"> <strong>AI FOR FUN </strong></font>
+            <br></br>
+            <font face="Verdana" size="+2" color="white"> Our application aims to provide the best AI experience. </font>
+            <br></br>
+            <font face="Verdana" size="+2" color="white"> Enjoy the fun brought by AI technology via website</font>
+          </div>
+          <div>
+            <Button size="lg" href="/AI_face" style={{ marginTop: 20, marginRight: 10, backgroundColor: "rgb(69,113,182)", borderColor: 'rgba(0,0,0,0)' }}>AI Face</Button>{' '}
+            <Button size="lg" href="/AI_text" style={{ marginTop: 20, marginRight: 10, backgroundColor: "rgb(69,113,182)", borderColor: 'rgba(0,0,0,0)' }}>AI Voice</Button>{' '}
+            <Button size="lg" href="/AI_style" style={{ marginTop: 20, marginRight: 10, backgroundColor: "rgb(69,113,182)", borderColor: 'rgba(0,0,0,0)' }}>AI Style</Button>{' '}
+          </div>
         </div>
       </div>
     </div>
