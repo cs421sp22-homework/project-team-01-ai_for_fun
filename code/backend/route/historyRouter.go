8a2310796d0520ca5552dae93a7bfc808eff60c7
dongji1111 <33479512+dongji1111@users.noreply.github.com>
2022-04-22 23:21:32 -0400

    Update historyRouter.go

--- code/backend/route/historyRouter.go
+++ code/backend/route/historyRouter.go
@@ -1,9 +1,12 @@
 package route
+
 import (
 	"github.com/cs421sp22-homework/project-team-01-ai_for_fun/controller"
 	"github.com/gin-gonic/gin"
 )
+
 func HistoryRoutes(router *gin.Engine) {
 	router.GET("/gethistory/:user_id", controller.Gethis())
+	router.GET("/gethistory/:user_id/:type", controller.GetTypeHistory())
 	router.POST("/deletehistory", controller.Deletehis())
 }
