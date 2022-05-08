afe5075377a81f8f90db34ab1f293363e51a4cce
dongji1111 <765571197@qq.com>
2022-05-07 21:29:50 -0400

    merge frontend

---
+++ code/frontend/front/src/components/LikeBtn.js
@@ -0,0 +1,55 @@
+import {LikeOutlined,LikeFilled} from '@ant-design/icons';
+import React from 'react';
+
+
+class LikeBtn extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            disable:props.props.disable,
+            like:props.props.times,
+            liked:props.props.liked,
+        };
+    }
+    islike =()=>{
+        if (this.state.disable == true){
+            return;
+        }
+        let liked=this.state.liked;
+        if(liked){
+            if(liked===true){
+            this.setState({liked:false}) 
+            this.setState({like:this.state.like-1});
+        }
+            else
+            {
+                this.setState({liked:true});
+                this.setState({ like:this.state.like+1,});
+                
+            }     
+    }
+    else {
+        this.setState({
+            like:this.state.like+1,
+        });
+        this.setState({liked:true});
+    }
+};
+     render(){
+        
+         return(
+               <div style={
+                 {float:"left"}}>
+                    <span onClick={this.islike}>
+                    {React.createElement(this.state.liked===true ?
+                     LikeFilled 
+                     : 
+                     LikeOutlined
+                     )}
+                     </span>
+                {this.state.like}
+                </div> 
+         );
+     }
+ }
+ export default LikeBtn;
